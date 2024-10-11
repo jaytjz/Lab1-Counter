c8885660f5efe6d8cdb8cb48b793fe275b741c11
jaytjz <jeremytanjanzhe@gmail.com>
2024-10-11 11:14:06 +0100

    done task 1

--- task1/counter.sv
+++ task1/counter.sv
@@ -1,15 +1,16 @@
 module counter #(
     parameter WIDTH = 8
 ) (
-    // interface signals
     input logic     clk,
-    input logic     clk,
+    input logic     rst,
     input logic     en,
     output logic [WIDTH-1:0] count
-)
+);
 
-alwyas_ff @ (posedge clk)
-    if (rst) count <= {WIDTH{1'b0}};
-    else     count <= count + {{WIDTH-1{1'b0}, en}};
+always_ff @ (posedge clk)
+    if (rst) 
+        count <= {WIDTH{1'b0}};      // Reset the counter to 0
+    else 
+        count <= count + { {(WIDTH-1){1'b0}}, en }; // Correct replication and concatenation
 
 endmodule
